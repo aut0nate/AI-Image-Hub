aff6f288acfa907b874df8120a74fee437650dc3
Nathan Darker <100093337+aut0nate@users.noreply.github.com>
2026-05-08 17:09:00 +0100

    Add image sharing actions

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,8 @@ const nextConfig = {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [360, 480, 640, 750, 828, 1080, 1200],
     imageSizes: [32, 48, 64, 96, 128, 256, 384],
-    minimumCacheTTL: 31536000
+    minimumCacheTTL: 31536000,
+    qualities: [75, 78, 86]
   }
 };
 
